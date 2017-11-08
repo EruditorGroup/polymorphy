62775752b470eff0dd912cf7a4f09b2a95a23c80
Elia Schelokov <thaumant@gmail.com>
2017-11-08 12:53:58 +0300

    Add test for word constrain method

--- tests/word.py
+++ tests/word.py
@@ -1,5 +1,6 @@
 from unittest import main, TestCase, skip
 from polymorphy import Word
+from polymorphy.constants import nomn, accs
 
 
 class TestWord(TestCase):
@@ -8,6 +9,14 @@ class TestWord(TestCase):
         self.assertEqual(len(word.variants), 2)
         self.assertEqual(word.__repr__(), 'зеленый×2')
         self.assertEqual(word.variants[0].normal_form, 'зелёный')
+
+    def test_constrain(self):
+        word = Word('стол')
+        constrained = word.constrain(nomn)
+        self.assertEqual(len(word.variants), 2)
+        self.assertEqual(len(constrained.variants), 1)
+        self.assertTrue(any(accs in v.tag.grammemes for v in word.variants))
+        self.assertFalse(any(accs in v.tag.grammemes for v in constrained.variants))
 
 
 if __name__ == '__main__':
